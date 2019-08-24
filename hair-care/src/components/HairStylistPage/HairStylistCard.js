85dadd08958cd8ba6ef73ee7dece65c7feaa4643
Rockey Simmons <codenamerockey@gmail.com>
2019-08-24 12:49:41 -0700

    added folders, and components for hairstylist, also added dependencies for semantic ui and aaxios

---
+++ hair-care/src/components/HairStylistPage/HairStylistCard.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Card, Icon, Image } from 'semantic-ui-react';
+
+const HairStylistCard = props => {
+  const {} = props.stylist;
+  return (
+    <div class="ui card">
+      <div class="image">
+        <img src={image} />
+      </div>
+      <div class="content">
+        <a class="header">{name}</a>
+        <div class="meta">
+          <span class="date">{shop}</span>
+        </div>
+        <div class="description">{type}</div>
+        <div class="description">{status}</div>
+      </div>
+      <div class="extra content">
+        <a>
+          <i class="user icon" />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default HairStylistCard;
---
+++ hair-care/src/components/HairStylistPage/HairStylistMainPage.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+import HairStylistCard from './HairStylistCard';
+import axios from 'axios';
+const HairStylistMainPage = () => {
+  // TODO-1: Add useState to track data from useEffect
+  const [hairstylist, setHairStylist] = useState([]);
+
+  // always console.log  the data you are working with. So you can understand how to use that data.
+  console.log(hairstylist);
+
+  //  Add API Request here - must run in `useEffect`
+  //  Important: verify the 'useEffect' parameter: the dependancies array!
+
+  //Huge List of data, that's it. We loop through lists, and that is what I do here. After I loop through the list from the api I pass that data to the Hair Stylist Card component via props. So this entire Hair Stylist component returns a new array, of the Hair Stylist Card component with the data looped over each time for each item in that array. That is what this components code RESOLVES TO.
+
+  //   useEffect(() => {
+  //     axios
+  //       .get('')
+  //       .then(response => {
+  //         // console.log(response); waiting on api from backend
+  //         // setHairStylist(response);
+  //       })
+  //       .catch(error => {
+  //         console.error('Server Error', error);
+  //       });
+  //   }, []);
+
+  return (
+    <section className="character-list grid-view">
+      {hairstylist.map(stylist => {
+        console.log(stylist);
+        return <HairStylistCard key={stylist.id} stylist={stylist} />;
+      })}
+    </section>
+  );
+};
+
+export default HairStylistMainPage;
