eb2d9aff4495c4c62306fe77bc534e401ce1e43b
Adrian-Garcia <agarc5@hotmail.com>
2019-08-26 14:04:40 -0500

    edited app.js and working on hairStylist component

--- hair-care/src/App.js
+++ hair-care/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
+import Logo from '../src/haircare.png';
+import HairStylistMainPage from './components/HairStylistPage/HairStylistMainPage';
+import HairStylistPortfolio from './components/HairStylistPortfolioPage/HairStylistPortfolio';
+import HairStylistStylesPage from './components/HairStylistPortfolioPage/HairStylistStylesPage';
 
 function App() {
   return (
-    <div className="App">
-      <h1>App Component</h1>
+    <div className="App ">
+      <img src={Logo} alt="hair logo" />
+
+      <Route exact path={`/`} component={HairStylistMainPage} />
+      <Route path={`/portfolio`} component={HairStylistPortfolio} />
+      <Route path={`/stylespage`} component={HairStylistStylesPage} />
+      <Route path="/hairstylistpage" component={HairStylistStylesPage} />
     </div>
   );
 }
