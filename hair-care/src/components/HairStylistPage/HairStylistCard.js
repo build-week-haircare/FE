bae01242dd4a701a49092d1dc41cbdaff4e49336
Adrian-Garcia <agarc5@hotmail.com>
2019-08-27 15:57:38 -0500

    fixed error messages from console :)

--- hair-care/src/components/HairStylistPage/HairStylistCard.js
+++ hair-care/src/components/HairStylistPage/HairStylistCard.js
@@ -1,208 +1,208 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import stylists from './HairStylistsImages/Stylists'
+import stylists from './HairStylistImages';
 
 const HairStylistCard = props => {
   
   return (
-    <div class="ui link cards">
+    <div className="ui link cards">
 
-      <div class="card">
-        <div class="image">
-          <img src=""/>
+      <div className="card">
+        <div className="image">
+          <img src="https://files.slack.com/files-pri/T4JUEB3ME-FMKDLFLTT/test2.jpg" alt="stylist 1"/>
         </div>
 
-        <div class="content">
-            <span class="header">Name</span>
-            <div class="meta">
-              <span class="date">Salon</span>
+        <div className="content">
+            <span className="header">Name</span>
+            <div className="meta">
+              <span className="date">Salon</span>
             </div>
-            <div class="description">Years Styling: 8 </div>
-            <div class="description">Status: Available On Request</div>
+            <div className="description">Years Styling: 8 </div>
+            <div className="description">Status: Available On Request</div>
         </div>
 
-        <div class="extra content">
-            <button class="ui icon button">
-              <i class="thumbs up outline icon"></i>
+        <div className="extra content">
+            <button className="ui icon button">
+              <i className="thumbs up outline icon"></i>
             </button>
             <Link to="/portfolio">View Portfolio</Link>
-            <button class="ui icon button">
-              <i class="thumbs down outline icon"></i>
+            <button className="ui icon button">
+              <i className="thumbs down outline icon"></i>
             </button>
         </div>
       </div>
 
-      <div class="card">
-        <div class="image">
-          <img src=""/>
+      <div className="card">
+        <div className="image">
+          <img src="https://files.slack.com/files-pri/T4JUEB3ME-FMEBMV9EE/test6.jpg" alt="stylist 2"/>
         </div>
 
-        <div class="content">
-            <span class="header">Name</span>
-            <div class="meta">
-              <span class="date">Salon</span>
+        <div className="content">
+            <span className="header">Name</span>
+            <div className="meta">
+              <span className="date">Salon</span>
             </div>
-            <div class="description">Years Styling: 8 </div>
-            <div class="description">Status: Available On Request</div>
+            <div className="description">Years Styling: 8 </div>
+            <div className="description">Status: Available On Request</div>
         </div>
 
-        <div class="extra content">
-            <button class="ui icon button">
-              <i class="thumbs up outline icon"></i>
+        <div className="extra content">
+            <button className="ui icon button">
+              <i className="thumbs up outline icon"></i>
             </button>
             <Link to="/portfolio">View Portfolio</Link>
-            <button class="ui icon button">
-              <i class="thumbs down outline icon"></i>
+            <button className="ui icon button">
+              <i className="thumbs down outline icon"></i>
             </button>
         </div>
       </div>
 
-      <div class="card">
-        <div class="image">
-          <img src=""/>
+      <div className="card">
+        <div className="image">
+          <img src="https://files.slack.com/files-pri/T4JUEB3ME-FMEBRNSKD/test1.jpg" alt="stylist 3"/>
         </div>
 
-        <div class="content">
-            <span class="header">Name</span>
-            <div class="meta">
-              <span class="date">Salon</span>
+        <div className="content">
+            <span className="header">Name</span>
+            <div className="meta">
+              <span className="date">Salon</span>
             </div>
-            <div class="description">Years Styling: 8 </div>
-            <div class="description">Status: Available On Request</div>
+            <div className="description">Years Styling: 8 </div>
+            <div className="description">Status: Available On Request</div>
         </div>
 
-        <div class="extra content">
-            <button class="ui icon button">
-              <i class="thumbs up outline icon"></i>
+        <div className="extra content">
+            <button className="ui icon button">
+              <i className="thumbs up outline icon"></i>
             </button>
             <Link to="/portfolio">View Portfolio</Link>
-            <button class="ui icon button">
-              <i class="thumbs down outline icon"></i>
+            <button className="ui icon button">
+              <i className="thumbs down outline icon"></i>
             </button>
         </div>
       </div>
 
-      <div class="card">
-        <div class="image">
-          <img src=""/>
+      <div className="card">
+        <div className="image">
+          <img src="https://files.slack.com/files-pri/T4JUEB3ME-FMEBMV9FU/test5.jpg" alt="stylist 4"/>
         </div>
 
-        <div class="content">
-            <span class="header">Name</span>
-            <div class="meta">
-              <span class="date">Salon</span>
+        <div className="content">
+            <span className="header">Name</span>
+            <div className="meta">
+              <span className="date">Salon</span>
             </div>
-            <div class="description">Years Styling: 8 </div>
-            <div class="description">Status: Available On Request</div>
+            <div className="description">Years Styling: 8 </div>
+            <div className="description">Status: Available On Request</div>
         </div>
 
-        <div class="extra content">
-            <button class="ui icon button">
-              <i class="thumbs up outline icon"></i>
+        <div className="extra content">
+            <button className="ui icon button">
+              <i className="thumbs up outline icon"></i>
             </button>
             <Link to="/portfolio">View Portfolio</Link>
-            <button class="ui icon button">
-              <i class="thumbs down outline icon"></i>
+            <button className="ui icon button">
+              <i className="thumbs down outline icon"></i>
             </button>
         </div>
       </div>
 
-      <div class="card">
-        <div class="image">
-          <img src=""/>
+      <div className="card">
+        <div className="image">
+          <img src="https://files.slack.com/files-pri/T4JUEB3ME-FMEBUC8RH/test4.jpg" alt="stylist 5"/>
         </div>
 
-        <div class="content">
-            <span class="header">Name</span>
-            <div class="meta">
-              <span class="date">Salon</span>
+        <div className="content">
+            <span className="header">Name</span>
+            <div className="meta">
+              <span className="date">Salon</span>
             </div>
-            <div class="description">Years Styling: 8 </div>
-            <div class="description">Status: Available On Request</div>
+            <div className="description">Years Styling: 8 </div>
+            <div className="description">Status: Available On Request</div>
         </div>
 
-        <div class="extra content">
-            <button class="ui icon button">
-              <i class="thumbs up outline icon"></i>
+        <div className="extra content">
+            <button className="ui icon button">
+              <i className="thumbs up outline icon"></i>
             </button>
             <Link to="/portfolio">View Portfolio</Link>
-            <button class="ui icon button">
-              <i class="thumbs down outline icon"></i>
+            <button className="ui icon button">
+              <i className="thumbs down outline icon"></i>
             </button>
         </div>
       </div>
 
-      <div class="card">
-        <div class="image">
-          <img src=""/>
+      <div className="card">
+        <div className="image">
+          <img src="https://files.slack.com/files-pri/T4JUEB3ME-FMEBQJFMG/test10.jpg" alt="stylist 6"/>
         </div>
 
-        <div class="content">
-            <span class="header">Name</span>
-            <div class="meta">
-              <span class="date">Salon</span>
+        <div className="content">
+            <span className="header">Name</span>
+            <div className="meta">
+              <span className="date">Salon</span>
             </div>
-            <div class="description">Years Styling: 8 </div>
-            <div class="description">Status: Available On Request</div>
+            <div className="description">Years Styling: 8 </div>
+            <div className="description">Status: Available On Request</div>
         </div>
 
-        <div class="extra content">
-            <button class="ui icon button">
-              <i class="thumbs up outline icon"></i>
+        <div className="extra content">
+            <button className="ui icon button">
+              <i className="thumbs up outline icon"></i>
             </button>
             <Link to="/portfolio">View Portfolio</Link>
-            <button class="ui icon button">
-              <i class="thumbs down outline icon"></i>
+            <button className="ui icon button">
+              <i className="thumbs down outline icon"></i>
             </button>
         </div>
       </div>
 
-      <div class="card">
-        <div class="image">
-          <img src=""/>
+      <div className="card">
+        <div className="image">
+          <img src="https://files.slack.com/files-pri/T4JUEB3ME-FMEBUC90B/test12.jpg" alt="stylist 7"/>
         </div>
 
-        <div class="content">
-            <span class="header">Name</span>
-            <div class="meta">
-              <span class="date">Salon</span>
+        <div className="content">
+            <span className="header">Name</span>
+            <div className="meta">
+              <span className="date">Salon</span>
             </div>
-            <div class="description">Years Styling: 8 </div>
-            <div class="description">Status: Available On Request</div>
+            <div className="description">Years Styling: 8 </div>
+            <div className="description">Status: Available On Request</div>
         </div>
 
-        <div class="extra content">
-            <button class="ui icon button">
-              <i class="thumbs up outline icon"></i>
+        <div className="extra content">
+            <button className="ui icon button">
+              <i className="thumbs up outline icon"></i>
             </button>
             <Link to="/portfolio">View Portfolio</Link>
-            <button class="ui icon button">
-              <i class="thumbs down outline icon"></i>
+            <button className="ui icon button">
+              <i className="thumbs down outline icon"></i>
             </button>
         </div>
       </div>
 
-      <div class="card">
-        <div class="image">
-          <img src=""/>
+      <div className="card">
+        <div className="image">
+          <img src="https://files.slack.com/files-pri/T4JUEB3ME-FMRR2MP1B/test11.jpg" alt="stylist 8"/>
         </div>
 
-        <div class="content">
-            <span class="header">Name</span>
-            <div class="meta">
-              <span class="date">Salon</span>
+        <div className="content">
+            <span className="header">Name</span>
+            <div className="meta">
+              <span className="date">Salon</span>
             </div>
-            <div class="description">Years Styling: 8 </div>
-            <div class="description">Status: Available On Request</div>
+            <div className="description">Years Styling: 8 </div>
+            <div className="description">Status: Available On Request</div>
         </div>
 
-        <div class="extra content">
-            <button class="ui icon button">
-              <i class="thumbs up outline icon"></i>
+        <div className="extra content">
+            <button className="ui icon button">
+              <i className="thumbs up outline icon"></i>
             </button>
             <Link to="/portfolio">View Portfolio</Link>
-            <button class="ui icon button">
-              <i class="thumbs down outline icon"></i>
+            <button className="ui icon button">
+              <i className="thumbs down outline icon"></i>
             </button>
         </div>
       </div>
--- hair-care/src/components/HairStylistPage/HairStylistMainPage.js
+++ hair-care/src/components/HairStylistPage/HairStylistMainPage.js
@@ -31,12 +31,16 @@ const HairStylistMainPage = () => {
   return (
     <section className="character-list grid-view">
       <div className="ui text container">
+      <div className="ui divider"></div>
         <h2>
           We provide the best and fastest way to find a great hairstylist.
           Getting the trendiest haircut with confidence. <br /> “You need a
           haircut? We got you’re haircut.”
         </h2>
       </div>
+       
+      <h3 className="ui dividing header"></h3>
+      <h1>Find Stylists</h1>
       <HairStylistCard />;
       {/* {hairstylist.map(stylist => {
         console.log(stylist);
